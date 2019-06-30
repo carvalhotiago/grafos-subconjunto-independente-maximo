cbec1e7f9732f10627e127ddaad457a4841488f0
carvalhotiago <tiago.lfdc@gmail.com>
2019-06-29 22:25:26 -0300

    commit inicial

---
+++ runScripts.ps1
@@ -0,0 +1,50 @@
+.\grafos.exe Instancia/instanciaPequena1.bat saidaInstanciaPequena1Guloso.csv 1 0 0 0 0;
+.\grafos.exe Instancia/instanciaPequena2.bat saidaInstanciaPequena2Guloso.csv 1 0 0 0 0;
+.\grafos.exe Instancia/instanciaPequena3.bat saidaInstanciaPequena3Guloso.csv 1 0 0 0 0; 
+.\grafos.exe Instancia/instanciaMedia1.bat saidaInstanciaMedia1Guloso.csv 1 0 0 0 0; 
+.\grafos.exe Instancia/instanciaMedia2.bat saidaInstanciaMedia2Guloso.csv 1 0 0 0 0; 
+.\grafos.exe Instancia/instanciaMedia3.bat saidaInstanciaMedia3Guloso.csv 1 0 0 0 0; 
+.\grafos.exe Instancia/instanciaMedia4.bat saidaInstanciaMedia4Guloso.csv 1 0 0 0 0; 
+.\grafos.exe Instancia/instanciaGrande1.bat saidaInstanciaGrande1Guloso.csv 1 0 0 0 0; 
+.\grafos.exe Instancia/instanciaGrande2.bat saidaInstanciaGrande2Guloso.csv 1 0 0 0 0; 
+.\grafos.exe Instancia/instanciaGrande3.bat saidaInstanciaGrande3Guloso.csv 1 0 0 0 0; 
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena1.bat saidaInstanciaPequena1GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena2.bat saidaInstanciaPequena2GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena3.bat saidaInstanciaPequena3GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia1.bat saidaInstanciaMedia1GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia2.bat saidaInstanciaMedia2GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia3.bat saidaInstanciaMedia3GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia4.bat saidaInstanciaMedia4GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande1.bat saidaInstanciaGrande1GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande2.bat saidaInstanciaGrande2GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande3.bat saidaInstanciaGrande3GulosoRandomizadoAlfa1.csv 0 1 0 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena1.bat saidaInstanciaPequena1GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena2.bat saidaInstanciaPequena2GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena3.bat saidaInstanciaPequena3GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia1.bat saidaInstanciaMedia1GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia2.bat saidaInstanciaMedia2GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia3.bat saidaInstanciaMedia3GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia4.bat saidaInstanciaMedia4GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande1.bat saidaInstanciaGrande1GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande2.bat saidaInstanciaGrande2GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande3.bat saidaInstanciaGrande3GulosoRandomizadoAlfa2.csv 0 0 1 0 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena1.bat saidaInstanciaPequena1GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena2.bat saidaInstanciaPequena2GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena3.bat saidaInstanciaPequena3GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia1.bat saidaInstanciaMedia1GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia2.bat saidaInstanciaMedia2GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia3.bat saidaInstanciaMedia3GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia4.bat saidaInstanciaMedia4GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande1.bat saidaInstanciaGrande1GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande2.bat saidaInstanciaGrande2GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande3.bat saidaInstanciaGrande3GulosoRandomizadoAlfa3.csv 0 0 0 1 0; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena1.bat saidaInstanciaPequena1GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena2.bat saidaInstanciaPequena2GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaPequena3.bat saidaInstanciaPequena3GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia1.bat saidaInstanciaMedia1GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia2.bat saidaInstanciaMedia2GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia3.bat saidaInstanciaMedia3GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaMedia4.bat saidaInstanciaMedia4GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande1.bat saidaInstanciaGrande1GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande2.bat saidaInstanciaGrande2GulosoReativo.csv 0 0 0 0 1; }
+for($i=0;$i -le 30;$i++){ .\grafos.exe Instancia/instanciaGrande3.bat saidaInstanciaGrande3GulosoReativo.csv 0 0 0 0 1; }
